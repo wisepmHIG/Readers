ab4b9a4c64c86aac0524612ccf8f98088ca37396
Gilyong Shin <yenilgop@icloud.com>
2017-01-31 11:59:34 +0900

    Initial commit

---
+++ readers.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+//비밀독서단 팀 문제
+
+
+// 합계 구하기
+let numbers1 = [23, 11, 4, 63, 19, 34, 11, 3342, 1783, 46826963]
+var sum1: Int = 0
+for number in numbers1 {
+    sum1 += number
+}
+print(sum1)
+
+
+// 44보다 큰 수 찾기
+let numbers2 = [24, 17, 93, 57, 21]
+var largerThan44 = [Int]()
+for number in numbers2 {
+    if number >= 44 {
+        largerThan44.append(number)
+    }
+}
+print(largerThan44)
+
+
+//50 이하의 수 찾기
+
+let examInts = [81, 32, 11, 5, 16, 99, 88, 44]
+var solveInts: [Int] = []
+
+for item in examInts {
+    if item < 50 {
+        solveInts.append(item)
+    }
+}
+
+print(solveInts)
+
+
+
+//50 이하의 수에서 4의 배수 찾기
+
+var solveInts2: [Int] = []
+for item in examInts {
+    if item < 50 {
+        if item % 4 == 0{
+            solveInts2.append(item)
+        }
+    }
+}
+print(solveInts2)
